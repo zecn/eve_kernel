69de6e7508b244eb6be0e476ffc3b491851564b5
zecn <zecn.ichthus@gmail.com>
2013-06-05 01:40:45 +0700

    second commit

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 25 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#25 PREEMPT Fri May 31 14:34:10 WIT 2013"
+#define LINUX_COMPILE_TIME "14:34:10"
+#define LINUX_COMPILE_BY "kafir"
+#define LINUX_COMPILE_HOST "illegal-devshit"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-188) "
